6389940547f4074659723f79517bcfb003af4285
Wasi khan <wasikhan7099@gmail.com>
2024-08-28 09:51:44 -0700

    websitebDone

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import Button from "./Button"
+function Header({header}) {
+
+    return(
+        <header className="text-gray-600 body-font">
+    <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <img src={header.logo} className='h-12 w-12 rounded-full'  />
+        <span className="ml-3 text-xl">{header.title  }</span>
+      </a>
+      <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-space-around">
+        <a className="mr-5 hover:text-gray-900">Home</a>
+        <a className="mr-5 hover:text-gray-900">About us</a>
+        <a className="mr-5 hover:text-gray-900">Contact</a>
+        <a className="mr-5 hover:text-gray-900">Servics</a>
+      </nav>
+      <Button bgcolor="bg-white-400" b lable='Login'/>
+    </div>
+  </header>)
+    
+}
+export default Header
--- src/constant/langinpag.ts
+++ src/constant/langinpag.ts
@@ -57,3 +57,7 @@ const landingPageData = {
     },
   };
   export default landingPageData;
+
+
+
+
